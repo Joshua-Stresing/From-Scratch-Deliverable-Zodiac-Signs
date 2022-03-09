753e18bee01027c5b54e55f07da0d1aa44467dca
Joshua Stresing <pyrotorch69@yahoo.com>
2022-03-09 10:44:56 -0800

    founds the pic issue

--- src/components/Zodiac/Zodiac.js
+++ src/components/Zodiac/Zodiac.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Zodiac({ name, dates }) {
   return (
     <div className="zodiac">
-      <img alt={name} src={`${process.env.PUBLIC_URL}/images/${name}.png`} />
+      <img alt={name} src={`${process.env.PUBLIC_URL}/pics/${name}.png`} />
       <span className="name">{name}</span>
       <span>{dates}</span>
     </div>
